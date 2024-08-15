3d9887a2b7f1b2cdeaeeded7422843dcf4c52a60
Arbic <arbic@Arbics-MacBook-Air.local>
2024-08-15 16:30:46 +0300

    seerch from root and appear

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,18 @@
 import { Book } from 'lucide-react'
-import Link from 'next/link'
 import SelectFont from './fontChanger/SelectFont'
 import DarkLightModeBtn from './modeChange/DarkLightModeBtn'
 
 export function Header() {
+	const handleResetClick = () => {
+		window.location.href = '/i'
+	}
+
 	return (
 		<header>
 			<div className='w-full flex justify-between items-center'>
-				<Link href={'/i'}>
-					{' '}
+				<button onClick={handleResetClick}>
 					<Book className='text-4xl text-gray-400 w-20 h-10' />
-				</Link>
+				</button>
 
 				<div className='flex gap-4 items-center'>
 					<SelectFont />
