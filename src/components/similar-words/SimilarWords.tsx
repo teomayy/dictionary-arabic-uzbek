928365425f57b0a37124b6906db395b2f8468200
teomayy <teo.maybebaby@gmail.com>
2024-08-29 21:20:30 +0300

    some adds

--- src/components/similar-words/SimilarWords.tsx
+++ src/components/similar-words/SimilarWords.tsx
@@ -1,18 +1,39 @@
 'use client'
 
+import { DASHBOARD_PAGES } from '@/config/pages-url.config'
 import { IWord } from '@/models/dictionary'
 import { useStore } from '@/store/useStore'
-import { Bookmark } from 'lucide-react'
+import { addBookmark, isBookmarked, removeBookmark } from '@/utils/isBookmarked'
+import { BookmarkIcon } from 'lucide-react'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 interface SimilarWordsProps {
 	words: IWord[]
 }
 
 const SimilarWords: React.FC<SimilarWordsProps> = ({ words }) => {
+	const [bookmarkedWords, setBookmarkedWords] = useState<number[]>([])
+
 	const { language } = useStore()
 	if (!words.length) {
 		return null
+	}
+	useEffect(() => {
+		const initialBookmarks = words
+			.map(word => word.id)
+			.filter(id => isBookmarked(id))
+		setBookmarkedWords(initialBookmarks)
+	}, [words])
+
+	const handleBookmarkClick = (id: number) => {
+		if (isBookmarked(id)) {
+			removeBookmark(id)
+			setBookmarkedWords(prev => prev.filter(bookmarkId => bookmarkId !== id))
+		} else {
+			addBookmark(id)
+			setBookmarkedWords(prev => [...prev, id])
+		}
 	}
 
 	return (
@@ -27,15 +48,25 @@ const SimilarWords: React.FC<SimilarWordsProps> = ({ words }) => {
 						>
 							<Link
 								className='w-full cursor-pointer flex justify-between'
-								href=''
+								href={`${DASHBOARD_PAGES.DESCRIPTION}${encodeURIComponent(
+									word.id
+								)}`}
 							>
 								<span>
 									{language === 'arabic'
 										? word.word
 										: getFirstWord(word.short_words)}
 								</span>
-								<Bookmark className='hover:text-[#149E53]' />
 							</Link>
+							<button onClick={() => handleBookmarkClick(word.id)}>
+								<BookmarkIcon
+									className={`h-6 w-6 hover:text-[#149E53] ${
+										bookmarkedWords.includes(word.id)
+											? 'fill-[#149E53] text-[#149E53]'
+											: ''
+									}`}
+								/>
+							</button>
 						</li>
 					))}
 				</ul>
